4bb80b8ca78048007792ed7c9c4bb204cf48dc3e
Akshay Nair <aks7aynair@gmail.com>
2022-12-13 12:41:44 +0400

    ver 1.3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Navbar from './components/navbar';
-import Info from './components/info';
 import React from "react";
-import News from "./components/news";
 function App() {
   return (
     <div>
--- src/components/resp.js
+++ src/components/resp.js
@@ -1,32 +1,36 @@
 import React from "react"
-
+// import nr from 'hr.jpeg';
 class Resp extends React.Component{
     constructor () {
         super()
         this.state = {
             desc:null,
+            bgimg:null
         };
     }
     decider() {
         if(this.props.desc == 'rainy'){
             this.setState({
-                desc:"Make sure to take your umbrella!! as it is rainy outside"
+                desc:"Make sure to take your umbrella!! as it is rainy outside",
+                bgimg:"hr.jpeg"
             })
         }
         else if(this.props.desc == 'light rain'){
             this.setState({
-                desc:"A bit of light rain outside but do take your umbrella just to be safe!!"
+                desc:"A bit of light rain outside but do take your umbrella just to be safe!!",
+                bgimg:"lr.jpg"
             })
         }
         else{
             this.setState({
-                desc:"Enjoy today's weather with beautiful clear skies !!!"
+                desc:"Enjoy today's weather with beautiful clear skies !!!",
+                bgimg:"sd.jpg"
             })
         }
     }
     render(){
         return (
-            <div className="resp1" onMouseEnter = {() => this.decider()}>
+            <div className="resp1" onMouseEnter = {() => this.decider()} style={{background:this.state.bgimg}}>
                 <p>{this.state.desc} </p>
             </div>
 
