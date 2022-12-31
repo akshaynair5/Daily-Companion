b166667fbc260de536df869350f78ada186024a8
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2022-12-31 20:25:30 +0400

    ver 1.41

--- server/index1.js
+++ server/index1.js
@@ -2,20 +2,40 @@ const express = require('express')
 const bod_parser = require('body-parser')
 const app = express()
 const mysql = require('mysql')
+const cors  = require('cors')
 
+
+app.use(cors());
 const db = mysql.createConnection({
     user:'root',
     host: 'localhost',
     password: null,
     database: 'daily-companion'
-})
+});
+
 db.connect(function(err){
     if(err){
-        console.log(err.code);
-        console.log(err.fatal);
-    } 
-})
+        console.log(err);
+    }
+    // app.post("http://localhost/phpmyadmin/index.php?route=/sql&db=daily-companion&table=users.dc&pos=0",(req, res) => {
+    // const name = req.body.name;
+    // const email = req.body.name;
+    // const country = req.body.name;
+    // const state = req.body.name;
 
+    //     db.query("INSERT INTO users.dc (name,email,country,state) VALUES (?,?,?,?)",[name,email,country,state],
+    //     (err,result) => {
+    //         if(err){
+    //             console.log('Error values not inserted');
+    //         } else{
+    //             res.send("Values inserted")
+    //         }
+    //     }
+    //     )
+    // })
+    
+
+})
 $query = 'SELECT * FROM `users.dc`'
 
 db.query($query , function(err,rows,fields){
@@ -24,24 +44,13 @@ db.query($query , function(err,rows,fields){
         console.log("An error occured");
         return;
     } 
-    console.log("Successful excecution",rows);
+     console.log("Successful excecution",rows);
+     app.get("",(req, res) => res.json(rows))
 })
-// app.post("/signup",(req, res) => {
-//     const name = req.body.name;
-//     const email = req.body.name;
-//     const country = req.body.name;
-//     const state = req.body.name;
 
-//     db.query("INSERT INTO users.dc (name,email,country,state) VALUES (?,?,?,?)",[name,email,country,state],
-//     (err,result) => {
-//         if(err){
-//             console.log('Error values not inserted');
-//         } else{
-//             res.send("Values inserted")
-//         }
-//     }
-//     )
-// })
-app.listen(3000 , () => {
+
+
+
+app.listen(3001 , () => {
     console.log('appy')
 });
