954fdf5168315b89071ca9b69b2b6caf553130fc
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-01-02 23:26:11 +0400

    1.43

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Navbar from './components/navbar';
 import React from "react";
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Logintable from './login.js';
+import {Logintable , Login} from './login.js';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Navbar />} />
         <Route path="/signup" element={<Logintable />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
--- src/login.js
+++ src/login.js
@@ -1,37 +1,68 @@
 import React from "react";
 import axios from "axios";
+import { Link } from 'react-router-dom';
 
 class Logintable extends React.Component{
     constructor(){
         super()
         this.state = {
             name:null,
+            password:null,
             email:null,
             country:null,
             state:null
         }
     }
-    postinfo(){
-        axios.post('http://localhost:3000/signup', {
-            name:this.state.name,
-            email:this.state.email,
-            country:this.state.country,
-            state:this.state.state
-        }).then (()=>{
-            console.log("patti")
-        })
+    postinfo = async (e) => {
+        e.preventDefault();
+        try{
+                await axios.post("http://localhost:3001/new/signup"  , {
+                    uname:this.state.name,
+                    upassword:this.state.password,
+                    uemail:this.state.email,
+                    ucountry:this.state.country,
+                    ustate:this.state.state
+            })
+        } catch(error){
+            console.log(error.response);
+        }
+
     }
     render(){
         return(
-            <div>
+            <div className="sform">
                 <input type="text" className="ipna" placeholder="Name" onChange={(e)=>{this.setState({name:e.target.value})}}></input>
+                <input type="text" className="ippa" placeholder="Password" onChange={(e)=>{this.setState({password:e.target.value})}}></input>
                 <input type="email" className="ipem" placeholder="E-mail" onChange={(e)=>{this.setState({email:e.target.value})}}></input>
                 <input type="text" className="ipco" placeholder="Country" onChange={(e)=>{this.setState({country:e.target.value})}}></input>
                 <input type="text" className="ipst" placeholder="State/Province" onChange={(e)=>{this.setState({state:e.target.value})}}></input>
-                <input type="button" onClick={() => this.postinfo()}></input>
+                <input type="button" onClick={(e) => this.postinfo(e)} value="Sign-Up"></input>
+                <p>Already a member?</p>
+                <Link to='/login' className="link1">click here to login</Link>
             </div>
         )
     }
 }
 
-export default Logintable
+class Login extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            email:null,
+            password:null
+        }
+    }
+    render(){
+        return(
+                <div className="sform">
+                    <input type="email" className="ipem" placeholder="E-mail" onChange={(e)=>{this.setState({email:e.target.value})}}></input>
+                    <input type="text" className="ippa" placeholder="Password" onChange={(e)=>{this.setState({password:e.target.value})}}></input>
+                    <input type="button" onClick={(e) => this.postinfo(e)} value="Sign-Up"></input>
+                    <p>Not a member?</p>
+                    <Link to='/new/signup' className="link1">click here to Signup</Link>
+                </div>
+        )
+    }
+}  
+
+export {Logintable , Login}
