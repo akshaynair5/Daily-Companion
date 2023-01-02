9f84b604ca5c9302bd7845959d718bacce7065c6
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-01-02 23:27:37 +0400

    1.43

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,6 +8,7 @@ class Navbar extends React.Component{
     constructor() {
         super()
         this.state = {
+            state:null,
             tempe:null,
             feels:null,
             humi:null,
@@ -20,6 +21,37 @@ class Navbar extends React.Component{
         };
 
     }
+    initial= () =>{
+        fetch()
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.state.inputText},&limit=5&appid=9413103f25a00a0e82d52d9040e82435`)
+            .then(response => response.json())
+            .then(data => {
+                const lati = data[0].lat;
+                const long = data[0].lon;
+                console.log(lati);
+                console.log(long);
+                fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lati}&lon=${long}&appid=9413103f25a00a0e82d52d9040e82435&units=imperial`)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                        this.setState ({
+                                tempe : data.list[0].main.temp,
+                                feels : data.list[0].main.feels_like,
+                                humi : data.list[0].main.humidity,
+                                tmax : data.list[0].main.temp_max,
+                                temp_min : data.list[0].main.temp_min,
+                                winds : data.list[0].wind.speed,
+                                deg : data.list[0].wind.deg,
+                                gus : data.list[0].wind.gust,
+                                coun : data.city.country,
+                                city : data.city.name,
+                                report : data.list[0].weather[0].description,
+                                main1 :data.list[0].weather[0].main
+                            })
+
+                })
+            })
+        }
     Loadcon() {
         // http://api.openweathermap.org/geo/1.0/direct?q=dubai,&limit=5&appid=9413103f25a00a0e82d52d9040e82435
         fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.state.inputText},&limit=5&appid=9413103f25a00a0e82d52d9040e82435`)
@@ -53,7 +85,7 @@ class Navbar extends React.Component{
         }
     render(){
         return( 
-            <nav className="navbar" onMouseEnter = { () => this.Loadcon()}>
+            <nav className="navbar" onLoad = {() =>this.initial()}onMouseEnter = { () => this.Loadcon()}>
                 <div className="container-fluid">
                     <p className="navbar-brand" ><b>Daily Companion</b></p>
                     <input className="ipbx" placeholder="Search City" aria-label="Search" id="sbar" onChange={(e) => this.setState({ inputText: e.target.value })} ></input>
