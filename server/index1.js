35c36f4d12b018b1313cacce2ab1b06c1ca946e9
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-01-03 23:29:19 +0400

    1.44

--- server/index1.js
+++ server/index1.js
@@ -8,15 +8,22 @@ const cors  = require('cors')
 app.use(cors());
 app.use(express.json())
 const db = mysql.createConnection({
-    user:'root',
+    user:'sqluser',
     host: 'localhost',
-    password: null,
-    database: 'daily-companion'
+    password: 'Playstation4',
+    database: 'daily-companion',
+    port:3307,
+    insecureAuth : true
 });
 
-// $query = 'SELECT * FROM `users.dc`'
-
-
+db.query("SELECT * FROM `users.dc`",(err,result)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(result)
+    }
+})
 
 app.post("/new/signup",(req,res) =>{
     const name = req.body.uname
@@ -28,7 +35,7 @@ app.post("/new/signup",(req,res) =>{
         "INSERT INTO `users.dc`(`name`, `password`, `email`, `country`, `state`) VALUES (?,?,?,?,?)",
         [name,password,email,country,state],(err,result) =>{
             if(err){
-                console.log(err);
+                console.log(res.err);
             }else{
                 console.log("success")
             }
@@ -36,12 +43,23 @@ app.post("/new/signup",(req,res) =>{
     )
 })
 
-// app.get("/data",(req,res)=>{
-//     const state = req.body.state
-//     db.query("SELECT state FROM ")
-// })
+app.post("/validation",(req,res)=>{
+    const email = req.body.email
+    const password = req.body.password
+    db.query("SELECT * FROM `users.dc` WHERE email= ? AND password= ?",[email,password],(err,result)=>{
+        if(err){
+            res.send({err :err});
+        }
+        if(result.length>0){
+            res.send(result);
+        }
+        else{
+            res.send({message:"Wrong username/password combination"})
+        }
+    })
+})
 
 
 app.listen(3001 , () => {
-    console.log('appy')
+    console.log('running on port 3001')
 });
