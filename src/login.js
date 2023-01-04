b2882a5bd8303d2eb175411e4abac9940b2d0474
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-01-04 23:58:28 +0400

    1.44

--- src/login.js
+++ src/login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 class Logintable extends React.Component{
     constructor(){
@@ -48,18 +48,39 @@ class Login extends React.Component{
     constructor(){
         super()
         this.state = {
+            username:null,
+            state:null,
+            country:null,
             email:null,
-            password:null
+            password:null,
+            state:null
         }
     }
+    validation = async (e) =>{
+        e.preventDefault();
+                await axios.post("http://localhost:3001/validation",{
+                email:this.state.email,
+                password:this.state.password
+            }).then((response)=>{
+                console.log(response.data)
+                this.setState({
+                    username:response.data[0].name,
+                    state:response.data[0].state,
+                    country:response.data[0].country
+                })
+            })
+    }
+    const nav = useNavigate()
     render(){
         return(
                 <div className="sform">
                     <input type="email" className="ipem" placeholder="E-mail" onChange={(e)=>{this.setState({email:e.target.value})}}></input>
                     <input type="text" className="ippa" placeholder="Password" onChange={(e)=>{this.setState({password:e.target.value})}}></input>
-                    <input type="button" onClick={(e) => this.postinfo(e)} value="Sign-Up"></input>
+                    <input type="button" onClick={(e) => this.validation(e)} value="Sign-Up"></input>
                     <p>Not a member?</p>
                     <Link to='/new/signup' className="link1">click here to Signup</Link>
+                    <input type="button" onClick={(e) => this.redir('/home')} value="Sign-Up"></input>
+
                 </div>
         )
     }
